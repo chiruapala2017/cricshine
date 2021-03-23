56aa899ea4afda838368e467a84ada3bb456f2b9
chiruapala2017 <chiru.cu05@gmail.com>
2021-03-23 09:26:58 +0530

    some changes

--- src/main/java/org/chiranjib/sbpractice/cricshine/CricshineApplication.java
+++ src/main/java/org/chiranjib/sbpractice/cricshine/CricshineApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class CricshineApplication {
+public class CricshineApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CricshineApplication.class, args);
